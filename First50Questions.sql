75a41e7942caf8de309707509593eddb0aa8044b
AK-5999 <87750898+AK-5999@users.noreply.github.com>
2025-09-27 15:27:19 +0530

    Update First50Questions.sql

--- First50Questions.sql
+++ First50Questions.sql
@@ -69,3 +69,178 @@ GROUP BY dept_id;
 SELECT dept_id, AVG(salary) AS AverageDepartmentSalary
 FROM Employees
 WHERE dept_id=3;
+
+-- 25. List total salary per department (use GROUP BY)
+
+SELECT SUM(salary) as TotalSalary, dept_id, dept_name
+FROM (
+SELECT *
+FROM Employees
+  JOIN Departments on Employees.dept_id = Departments.dept_id
+)
+GROUP BY dept_id
+
+
+-- 26. List departments with more than 4 employee (use HAVING)
+SELECT dept_name FROM (
+SELECT *
+FROM Employees
+  JOIN Departments on Employees.dept_id = Departments.dept_id
+) GROUP BY dept_name
+HAVING  COUNT(emp_name) > 4;
+--OR
+SELECT dept_name FROM(
+SELECT dept_name, COUNT(emp_name) as TotalEmployee FROM(
+SELECT *
+FROM Employees
+  JOIN Departments on Employees.dept_id = Departments.dept_id
+) GROUP BY dept_id
+) WHERE TotalEmployee > 4
+
+-- 27. Find departments where average salary > 60000
+SELECT dept_name FROM(
+SELECT *
+FROM Employees
+  JOIN Departments on Employees.dept_id = Departments.dept_id
+) GROUP BY dept_id
+HAVING AVG(salary)>60000
+
+-- 28. Count employees grouped by status
+SELECT COUNT(emp_id) FROM Employees
+GROUP BY status
+
+-- 29. List all employees with their department name
+
+SELECT emp_name,dept_name 
+FROM Employees
+JOIN Departments ON Employees.dept_id = Departments.dept_id 
+
+-- 30. List all projects with department name
+SELECT proj_name, dept_name FROM Projects
+JOIN Departments ON Projects.dept_id = Departments.dept_id
+
+-- 31. List employee name and project name (only if dept_id matches)
+SELECT emp_name, proj_name FROM Employees
+JOIN Projects ON Employees.dept_id =Projects.dept_id
+
+-- 32. List employees with department name where salary > 60000
+SELECT emp_name,dept_name,salary FROM Employees
+JOIN Departments ON Employees.dept_id = Departments.dept_id
+WHERE salary>60000
+
+-- 33. Left join departments with employees (include departments with no employees)
+SELECT * FROM Departments
+LEFT JOIN Employees ON Employees.dept_id = Departments.dept_id
+
+-- 34. Right join departments with employees (include employees with no department)
+SELECT * FROM Employees
+LEFT JOIN Departments ON Departments.dept_id = Employees.dept_id
+
+-- 35. Inner join employees and projects on dept_id
+SELECT * FROM Employees
+JOIN Projects on Employees.dept_id = Projects.dept_id
+
+--36. List employees whose salary > average salary
+
+SELECT emp_name,salary FROM Employees
+Where salary > (
+  SELECT AVG(salary) FROM Employees
+  )
+
+
+
+--37. List employees who work in the department with the maximum number of employees
+--SELECT dept_id FROM Employees
+--Where
+SELECT emp_name FROM Employees
+Where dept_id in (
+SELECT dept_id
+FROM (SELECT COUNT(emp_name) AS EmployeeCount, dept_id
+		FROM Employees
+  		GROUP BY dept_id)
+WHERE EmployeeCount = (
+	SELECT MAX(EmployeeCount) FROM (
+		SELECT COUNT(emp_name) AS EmployeeCount, dept_id
+		FROM Employees
+  		GROUP BY dept_id
+	)
+)
+)
+
+
+
+--38. List departments which have more than 2 projects
+SELECT dept_name FROM Departments
+WHERE dept_id = (
+SELECT dept_id FROM (
+	SELECT COUNT(proj_name) AS ProjectCount,dept_id FROM Projects
+  GROUP BY dept_id
+) WHERE ProjectCount > 2
+);
+
+--39. Find projects where department has salary sum > 300000
+SELECT dept_name from Departments
+WHERE dept_id in (
+SELECT dept_id FROM (
+	SELECT SUM(salary) as TotalSalary, dept_id FROM Employees
+  GROUP BY dept_id
+) WHERE TotalSalary > 300000
+)
+
+
+--40. Find employees working in the department of employee 'Riya Sharma'
+SELECT emp_name FROM (
+SELECT emp_name FROM Employees
+WHERE dept_id = (
+SELECT dept_id FROM Employees
+  WHERE emp_name = 'Riya Sharma'
+)
+)
+WHERE NOT(emp_name = 'Riya Sharma') ;
+
+--41. List employees whose names contain 'Sharma'
+SELECT emp_name from Employee
+WHERE emp_name LIKE '%Sharma%';
+
+--42. Convert employee names to uppercase
+SELECT
+    UPPER(emp_name) AS UppercaseName,
+    *
+FROM
+    Employees;
+
+--43. Extract year from hire_date
+SELECT
+emp_name,
+strftime('%Y', hire_date) AS HireYear
+FROM Employees;
+
+
+--44. List employees hired after 2025-01-01
+SELECT
+emp_name,hire_date
+FROM Employees
+WHERE hire_date > '2025-01-01';
+
+--45. Concatenate employee name and email in one column
+SELECT emp_name || ' (' || email || ')' AS NameEmail
+FROM Employees
+
+-- 46. Update employee status to 'Inactive' for salary < 50000
+
+UPDATE Employees
+SET status = 'Inactive'
+WHERE salary < 50000;
+-- 47. Change phone_number for emp_id = 101
+UPDATE Employees
+SET phone_number  = 98765432549
+WHERE emp_id=101;
+-- 48. Delete employees with salary < 48000
+DELETE FROM Employees
+WHERE salary < 48000;
+-- 49. Add a new column `bonus` to employees table
+ALTER TABLE employees
+ADD COLUMN bonus INT;
+
+-- 50. Truncate the projects table
+TRUNCATE TABLE employees;
